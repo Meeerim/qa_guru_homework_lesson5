178ae8875f5898630296e6a69e18c5175eebc4cc
Meerim Sabytkyzy <skmeerim1999@gmail.com>
2023-04-24 00:15:54 +0400

    second

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def browser_settings():
     chrome_options = webdriver.ChromeOptions()
     #chrome_options.add_argument('--headless')
     browser.config.window_width = 1400
-    browser.config.window_height = 800
+    browser.config.window_height = 700
 
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.config.timeout = 2.0
