5389c874c7984508ed96a9fb52b18866fcb3c0e7
Meerim Sabytkyzy <skmeerim1999@gmail.com>
2023-05-09 15:47:20 +0400

    Fix after code review

--- conftest.py
+++ conftest.py
@@ -1,14 +1,12 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
-    chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument('--headless')
     browser.config.window_width = 1400
     browser.config.window_height = 700
 
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 2.0
+
 
--- test_tools_for_qa.py
+++ test_tools_for_qa.py
@@ -1,26 +1,28 @@
+import os
 import time
-
-from selene import browser,have, be
+from selene import browser, have
 from selene.core import command
 
 
-
 def test_practice_form():
-    browser.open('/')
+    browser.open('/automation-practice-form')
     browser.element('#firstName').click().type('Meerim')
     browser.element('#lastName').click().type('Sabyt')
     browser.element('#userEmail').click().type('skmeerim1999@gmail.com')
-    browser.element(css_or_xpath_or_by=("label[for='gender-radio-2']")).click()
+    browser.element('[for="gender-radio-2"]').click()
     browser.element('#userNumber').click().type('0554803097')
     browser.element('#dateOfBirthInput').click()
-    browser.element(css_or_xpath_or_by=('[class="react-datepicker__year-select"]')).click()
+    browser.element('.react-datepicker__year-select').click()
     browser.element('[value="1999"]').click()
-    browser.element(css_or_xpath_or_by=('[class="react-datepicker__month-select"]')).click()
+    browser.element('.react-datepicker__month-select').click()
     browser.element('[value="9"]').click()
-    browser.element(css_or_xpath_or_by=('[class="react-datepicker__day react-datepicker__day--031 react-datepicker__day--weekend"]')).click()
-    browser.element(css_or_xpath_or_by=('#subjectsInput')).click().type('Ma').press_enter()
-    browser.element(css_or_xpath_or_by=("label[for='hobbies-checkbox-2']")).perform(command.js.scroll_into_view).click()
-    browser.element('#uploadPicture').send_keys(r"C:\Users\skmee\PycharmProjects\qa_guru_homework_lesson5\image\totoro.jpg")
+    browser.element('.react-datepicker__day--031').click()
+    browser.element('#subjectsInput').click().type('Ma').press_enter()
+    time.sleep(1)
+    browser.element("[for='hobbies-checkbox-2']").perform(command.js.scroll_into_view).click()
+    file_path = os.path.abspath(os.path.join('image', 'totoro.jpg'))
+    browser.element('#uploadPicture').send_keys(file_path)
+    time.sleep(1)
     browser.element('textarea#currentAddress').type("str Abd 123")
     browser.element('#state').click()
     browser.element('#react-select-3-option-2').click()
@@ -29,4 +31,14 @@ def test_practice_form():
     browser.element('#submit').press_enter()
 
     browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
-    browser.element('#closeLargeModal').perform(command.js.scroll_into_view).click()
+    browser.all('.table-responsive td:nth-child(2)').should((have.texts('Meerim Sabyt',
+                                                                        'skmeerim1999@gmail.com',
+                                                                        'Female',
+                                                                        '0554803097',
+                                                                        '31 October,1999',
+                                                                        'Maths',
+                                                                        'Reading',
+                                                                        'totoro.jpg',
+                                                                        'str Abd 123',
+                                                                        'Haryana Karnal')))
+
