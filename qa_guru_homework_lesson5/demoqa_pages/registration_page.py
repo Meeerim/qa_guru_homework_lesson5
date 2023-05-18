f0fe75769e667e75d16688253840b6e442e4de25
Meerim Sabytkyzy <skmeerim1999@gmail.com>
2023-05-18 20:19:32 +0400

    High level

--- qa_guru_homework_lesson5/demoqa_pages/registration_page.py
+++ qa_guru_homework_lesson5/demoqa_pages/registration_page.py
@@ -4,6 +4,7 @@ from selene import browser, have
 from selene.core import command
 
 from qa_guru_homework_lesson5 import resources
+from qa_guru_homework_lesson5.data.users import User, Gender, Hobbies
 
 
 class RegistrationFormPage:
@@ -25,23 +26,36 @@ class RegistrationFormPage:
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type(month)
         browser.element('.react-datepicker__year-select').type(year)
-        browser.element( f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
-        ).click()
+        browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+                        ).click()
         return self
+
     def fill_user_email(self, email):
         browser.element('#userEmail').click().type(email)
 
-    def select_gender(self):
-        browser.element('[for="gender-radio-2"]').click()
+    def select_gender(self, gender):
+        if gender == Gender.MALE:
+            browser.element('[for="gender-radio-1"]').click()
+        elif gender == Gender.FEMALE:
+            browser.element('[for="gender-radio-2"]').click()
+        else:
+            browser.element('[for="gender-radio-3"]').click()
+
 
     def fill_user_number(self, number):
         browser.element('#userNumber').type(number)
 
-    def select_subject(self, subject):
-        browser.element('#subjectsInput').type(subject).press_enter()
+    def select_subject(self,subject):
+        browser.element('#subjectsInput').perform(command.js.scroll_into_view).type(subject[0].value).press_enter()
 
-    def select_hobby(self):
-        browser.all('.custom-checkbox').element_by(have.text('Reading')).perform(command.js.scroll_into_view).click()
+    def select_hobby(self, hobby):
+        if hobby == Hobbies.SPORTS:
+            browser.element('[for="hobbies-checkbox-1"]').click()
+        elif hobby == Hobbies.READING:
+            browser.element('[for="hobbies-checkbox-2"]').click()
+        else:
+            browser.element('[for="hobbies-checkbox-3"]').click()
+
 
     def upload_file(self, file_name):
         browser.element('#uploadPicture').send_keys(resources.path(file_name))
@@ -50,13 +64,13 @@ class RegistrationFormPage:
     def fill_address(self, address):
         browser.element('textarea#currentAddress').type(address)
 
-    def fill_state(self):
+    def fill_state(self, state):
         browser.element('#state').click()
-        browser.element('#react-select-3-option-2').click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.text(state)).click()
 
-    def fill_city(self):
+    def fill_city(self, city):
         browser.element('#city').click()
-        browser.element('#react-select-4-option-0').click()
+        browser.all('[id^=react-select][id*=option]').element_by(have.text(city)).click()
 
     def click_submit(self):
         browser.element('#submit').press_enter()
@@ -64,7 +78,8 @@ class RegistrationFormPage:
     def registered_user_data(self):
         return browser.element('.table').all('td').even
 
-    def should_registered_user_with(self, full_name, email, gender,phone_number,birthday,subject,hobby,image,address,city):
+    def should_registered_user_with(self, full_name, email, gender, phone_number, birthday, subject, hobby, image,
+                                    address, city):
         browser.element('.table').all('td').even.should(
             have.exact_texts(
                 full_name,
@@ -80,3 +95,17 @@ class RegistrationFormPage:
             )
         )
         return self
+
+    def fill_user_info(self, user: User):
+        self.fill_first_name(user.first_name)
+        self.fill_last_name(user.last_name)
+        self.fill_user_email(user.email)
+        self.select_gender(user.gender)
+        self.fill_user_number(user.user_number)
+        self.fill_date_of_birth(user.birthday.year, user.birthday.month, user.birthday.day)
+        self.select_subject(user.subject)
+        self.select_hobby(user.hobby)
+        self.upload_file(user.image)
+        self.fill_address(user.address)
+        self.fill_state(user.state)
+        self.fill_city(user.city)
