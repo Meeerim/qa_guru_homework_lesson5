ec31e430fd4206f05b94febf6c03e08bece06017
Meerim Sabytkyzy <skmeerim1999@gmail.com>
2023-05-20 17:57:22 +0400

    Some changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selene import browser
 
+
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
     browser.config.window_width = 1400
@@ -8,5 +9,3 @@ def browser_settings():
 
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 2.0
-
-
